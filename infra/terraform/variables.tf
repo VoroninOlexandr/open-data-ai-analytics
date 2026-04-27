339bc8d8321ff067985dd6ae40cf07920c764b0b
mantix <146076479+VoroninOlexandr@users.noreply.github.com>
2026-04-27 21:37:55 +0300

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  default     = "northeurope"
+  description = "Регіон для розгортання"
+}
+
+variable "prefix" {
+  default     = "opendatav3"
+  description = "Префікс для назв ресурсів"
+}
